5a30faff86875d649e06254395f5fcd4c99ca460
RE045908 <re045908@qintess.com>
2025-05-03 11:20:34 -0300

    Models

---
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/domain/model/SensorMonitoring.java
@@ -0,0 +1,34 @@
+package com.algaworks.algasensors.temperature.monitoring.domain.model;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class SensorMonitoring {
+
+    @Id
+    @AttributeOverride(name = "value", column = @Column(name = "id", columnDefinition = "bigint"))
+    private SensorId id;
+
+    private Double lastTemperature;
+
+    private OffsetDateTime updateAt;
+
+    private Boolean enable;
+
+    public boolean isEnabled(){
+        return Boolean.TRUE.equals(enable);
+    }
+}
